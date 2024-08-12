2877156fd959dd6a54649013291a641d94013058
aishbagal <aishwarya.bagal@bosch.com>
2024-08-12 14:34:09 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,21 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+TEST(SoundexTestsuite, ConstantsReplacedByDigit) {
+    char soundex[5];
+    generateSoundex("BX", soundex);
+    ASSERT_STREQ(soundex, "B200");
+}
+ 
+TEST(SoundexTestsuite, NonAlphabeticCharactersIgnore) {
+    char soundex[5];
+    generateSoundex("A1X", soundex);
+    ASSERT_STREQ(soundex, "A200");
+}
+
+TEST(SoundexTestsuite, HandlesEmptyString) {
+    char soundex[5];
+    generateSoundex("", soundex);
+    ASSERT_STREQ(soundex, "0000");
+}
 }
